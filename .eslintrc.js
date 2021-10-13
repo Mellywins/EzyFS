7091f4a2625f28bd8391d42b65a37e7dac2a30ad
Oussema <oussemazouaghi@insat.u-carthage.tn>
2021-10-13 18:32:10 +0100

    eslint deps added

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'airbnb',
     'airbnb-typescript',
     'airbnb-base',
     'prettier',
