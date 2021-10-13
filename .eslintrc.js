4e74ac450053c52b9cfaef3c94f1303b99231519
Oussema <oussemazouaghi@insat.u-carthage.tn>
2021-10-13 18:31:52 +0100

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -9,12 +10,16 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'airbnb',
-    'airbnb-typescript'
+    'airbnb-typescript',
+    'airbnb-base',
+    'prettier',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
+    browser:true,
+    es6:true
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
@@ -22,5 +27,14 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'local',
+        args: 'none',
+      },
+    ],
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off',
   },
 };
---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM node:14.16.1-alpine3.11 As development
+
+LABEL maintainer="oussama.zouaghi@insat.ucar.tn"
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install 
+
+COPY . .
+
+RUN npm run build
+
+
+
+FROM node:14.16.1-alpine3.11 as production
+
+LABEL maintainer="oussama.zouaghi@insat.ucar.tn"
+ARG NODE_ENV=production
+ENV NODE_ENV=${NODE_ENV}
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install --only=production
+
+COPY . .
+
+
+COPY --from=development /usr/src/app/dist ./dist
+
+ENTRYPOINT [ "npm","run","start:prod" ]
