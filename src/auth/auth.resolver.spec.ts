a0428265acdeef33e5b90b72edde2a6041f14c15
Oussema <oussemazouaghi@insat.u-carthage.tn>
2021-10-14 19:54:30 +0100

    Auth module tests written

--- src/auth/auth.resolver.spec.ts
+++ src/auth/auth.resolver.spec.ts
@@ -1,19 +1,41 @@
 import {Test, TestingModule} from '@nestjs/testing';
+import {User} from '../user/entities/user.entity';
 import {AuthResolver} from './auth.resolver';
 import {AuthService} from './auth.service';
+import {CredentialsInput} from './dto/credentials.input';
+import {TokenModel} from './dto/token.model';
 
 describe('AuthResolver', () => {
   let resolver: AuthResolver;
-
+  const token: TokenModel = {
+    access_token: 'access_token',
+    refresh_token: 'refresh_token',
+    user: new User(),
+  };
+  const credentialsInput: CredentialsInput = {
+    username: 'ahmed grati',
+    password: 'pass123',
+  };
+  const mockAuthService = {
+    login: jest.fn().mockReturnValue(token),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthResolver, AuthService],
-    }).compile();
+    })
+      .overrideProvider(AuthService)
+      .useValue(mockAuthService)
+      .compile();
 
     resolver = module.get<AuthResolver>(AuthResolver);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('should perform a user login', () => {
+    expect(resolver.login(credentialsInput)).toMatchObject(token);
+    expect(mockAuthService.login).toBeCalledTimes(1);
   });
 });
