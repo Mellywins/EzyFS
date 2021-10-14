c6099c34edca06f4e21bc2d80d70448038f765bb
Oussema <oussemazouaghi@insat.u-carthage.tn>
2021-10-14 19:40:15 +0100

    user module providers fix

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,20 @@
 import {Module} from '@nestjs/common';
-import {UserService} from './user.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {AuthModule} from '../auth/auth.module';
+import {EmailModule} from '../email/email.module';
+import {RedisCacheModule} from '../redis-cache/redis-cache.module';
 import {UserResolver} from './user.resolver';
+import {UserService} from './user.service';
+import {User} from './entities/user.entity';
 
 @Module({
   providers: [UserResolver, UserService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    EmailModule,
+    RedisCacheModule,
+    AuthModule,
+  ],
+  exports: [UserService],
 })
 export class UserModule {}
