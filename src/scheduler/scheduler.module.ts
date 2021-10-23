81b6ca9da26132f26d35ebc026e0927c1e8a3d1e
Oussema <oussemazouaghi@insat.u-carthage.tn>
2021-10-23 22:09:44 +0100

    lint fix

--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { SchedulerService } from './scheduler.service';
-import { SchedulerResolver } from './scheduler.resolver';
+import {Module} from '@nestjs/common';
+import {SchedulerService} from './scheduler.service';
+import {SchedulerResolver} from './scheduler.resolver';
 
 @Module({
-  providers: [SchedulerResolver, SchedulerService]
+  providers: [SchedulerResolver, SchedulerService],
 })
 export class SchedulerModule {}
