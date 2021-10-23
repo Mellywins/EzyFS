755a86b035966e0eb28ac396e3a9ef897cdf1b60
Oussema <oussemazouaghi@insat.u-carthage.tn>
2021-10-23 22:56:04 +0100

    processors config in the scheduler module

--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -3,12 +3,23 @@ import {BullModule} from '@nestjs/bull';
 import {SchedulerService} from './scheduler.service';
 import {SchedulerResolver} from './scheduler.resolver';
 import {QueueType} from '../shared/enums/Queue.enum';
+import {ProcessorType} from '../shared/enums/Processor-types.enum';
+import {join} from 'path';
 
 @Module({
   providers: [SchedulerResolver, SchedulerService],
   imports: [
     BullModule.registerQueue({
       name: QueueType.COMPRESSION,
+      processors: [
+        {
+          name: ProcessorType.COMPRESSION,
+          path: join(
+            __dirname,
+            '/processors/compression/compression.processor.ts',
+          ),
+        },
+      ],
     }),
     BullModule.registerQueue({
       name: QueueType.ENCRYPTION,
---
+++ src/shared/enums/Processor-types.enum.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-shadow */
+export enum ProcessorType {
+  ENCRYPTION = 'ENCRYPTION',
+  DECRYPTION = 'DECRYPTION',
+  COMPRESSION = 'COMPRESSION',
+  DECOMPRESSION = 'DECOMPRESSION',
+}
