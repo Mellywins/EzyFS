f35ec8c716c489894d6a6202743ef84be3b09fa2
Oussema <oussemazouaghi@insat.u-carthage.tn>
2021-11-07 21:03:20 +0100

    scheduler entity deleted

--- src/scheduler/entities/scheduler.entity.ts
+++
@@ -1,7 +0,0 @@
-import {ObjectType, Field, Int} from '@nestjs/graphql';
-
-@ObjectType()
-export class Scheduler {
-  @Field(() => Int, {description: 'Example field (placeholder)'})
-  exampleField: number;
-}
