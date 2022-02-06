ee34313024d5b984b67fdd09ce0bbc0db80eec8b
Mellywins <zouaghi.wow1919@gmail.com>
2022-02-06 12:55:37 +0100

    currentUser Decorator for gql

---
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -0,0 +1,9 @@
+import {createParamDecorator, ExecutionContext} from '@nestjs/common';
+import {GqlExecutionContext} from '@nestjs/graphql';
+
+export const currentUser = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context).getContext();
+    return ctx.req.user;
+  },
+);
