a783099739b2ace6642a48167f95ba3d6b5b4b06
Mellywins <zouaghi.wow1919@gmail.com>
2022-02-06 15:27:23 +0100

    docker dev environment setup

--- apps/registration-authority/src/main.ts
+++ apps/registration-authority/src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { RegistrationAuthorityModule } from './registration-authority.module';
+import {NestFactory} from '@nestjs/core';
+import {RegistrationAuthorityModule} from './registration-authority.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(RegistrationAuthorityModule);
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
