23b3d8212883d8368e811afbe642f07de9907d55
Mellywins <zouaghi.wow1919@gmail.com>
2022-03-01 19:06:15 +0100

    consul pod setup with overwriten image to automatically register microservices config on start up

---
+++ k8s/consul/images/consul.Dockerfile
@@ -0,0 +1,14 @@
+FROM hashicorp/consul
+WORKDIR /usr/tmp/consul/configs
+RUN apk add jq bash
+COPY nest-cli.json .
+COPY apps/api-gateway/config.json ./api-gateway/config.json 
+COPY apps/notifications/config.json ./notifications/config.json 
+COPY apps/registration-authority/config.json ./registration-authority/config.json 
+
+COPY k8s/consul/service-register.sh ./service-register.sh
+RUN chmod +x ./service-register.sh
+RUN ls -l ./service-register.sh
+ENTRYPOINT ["bash","/usr/tmp/consul/configs/service-register.sh"]
+CMD ["agent", "-dev", "-client", "0.0.0.0"]
+
---
+++ k8s/consul/service-register.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# shellcheck disable=SC2207
+echo "HIII"
+exec /usr/local/bin/docker-entrypoint.sh "$@" &
+sleep 3
+CONFIG_DIR="/usr/tmp/consul/configs"
+PROJECTS=$(jq -r '.projects[] | select(.type == "application") | .root' ./nest-cli.json | sed  's/apps\///g')
+
+echo "Service Registration system started..."
+
+for PROJECT_NAME in ${PROJECTS} ; do
+    echo "${CONFIG_DIR}/${PROJECT_NAME}/config.json"
+    SVC_NAME="$( jq -r '.app.name' ${CONFIG_DIR}/${PROJECT_NAME}/config.json )"
+
+    echo "Registering ${SVC_NAME}"
+
+  if [ ! -f "${CONFIG_DIR}/${PROJECT_NAME}/config.json" ]; then
+    echo "${PROJECT_NAME}/${CONFIG_PATH} not found, skipping service"
+    continue
+  fi
+  
+    echo "**** ${PROJECT_NAME}"
+    echo " Saving config from path: ${CONFIG_DIR}/${PROJECT_NAME}/${CONFIG_PATH} to ezyfs/config/${SVC_NAME} "
+    consul kv put ezyfs/config/"${SVC_NAME}" \@"${CONFIG_DIR}/"${PROJECT_NAME}"/config.json"
+done
+consul monitor
